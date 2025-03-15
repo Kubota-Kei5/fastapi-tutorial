57fd12ff998b8d9ed4d05cbb2adbf00b6e7ebd31
kubotake-desktop <keigo1003560@gmail.com>
2025-03-15 02:38:45 +0000

    firestoreの環境構築

---
+++ snippets.py
@@ -0,0 +1,13 @@
+from firestore import db
+
+# doc_ref = db.collection("users").document("alovelace")
+# doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+# doc_ref = db.collection("users").document("aturing")
+# doc_ref.set({"first": "Alan", "middle": "Mathison", "last": "Turing", "born": 1912})
+
+users_ref = db.collection("users")
+docs = users_ref.stream()
+
+for doc in docs:
+    print(f"{doc.id} => {doc.to_dict()}")
